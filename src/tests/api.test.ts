19cf33c7cdab296ccbf6bc9554996d1ef12b4f6f
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-03 16:15:20 -0400

    Fix test

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -7,3 +7,14 @@ test('PharmaTech is running', async () => {
   const version = pharmaTech.version()
   expect(version).toBe('0.0.3')
 })
+
+test('AuthService login', async () => {
+  const pharmaTech = new PharmaTech()
+  const loginResponse = await pharmaTech.auth.login({
+    email: 'test@example.com',
+    password: 'password123',
+  })
+
+  expect(loginResponse).toBeDefined()
+  expect(loginResponse.accessToken).toBeDefined()
+})
