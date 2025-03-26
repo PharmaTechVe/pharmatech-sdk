c132662b31502850586c52f548fa4e027fd2e15d
Andres Alvarez <andres15alvarez@gmail.com>
2025-03-26 19:54:00 -0400

    Create inventory service

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { BranchService } from './branch'
 import { CategoryService } from './category'
 import { Client } from './client'
 import { CountryService } from './country'
+import { InventoryService } from './inventory'
 import { ManufacturerService } from './manufacturer'
 import { PresentationService } from './presentation'
 import { ProductService } from './product'
@@ -21,6 +22,7 @@ export class PharmaTech {
   category: CategoryService
   presentation: PresentationService
   manufacturer: ManufacturerService
+  inventory: InventoryService
 
   /**
    * @deprecated Use `PharmaTech.getInstance()` instead.
@@ -36,6 +38,7 @@ export class PharmaTech {
     this.category = new CategoryService(this.client)
     this.presentation = new PresentationService(this.client)
     this.manufacturer = new ManufacturerService(this.client)
+    this.inventory = new InventoryService(this.client)
   }
 
   static getInstance(isDevMode = false): PharmaTech {
