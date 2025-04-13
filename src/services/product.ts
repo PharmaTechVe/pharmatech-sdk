5979267b6c53f0992a954b9ccfcaa455a6d6bba9
Andres Alvarez <andres15alvarez@gmail.com>
2025-04-12 20:53:04 -0400

    Add filter in method

--- src/services/product.ts
+++ src/services/product.ts
@@ -16,10 +16,25 @@ export class ProductService {
     page,
     limit,
     q,
+    manufacturerId,
+    categoryId,
+    branchId,
+    presentationId,
+    priceRange,
   }: ProductPaginationRequest): Promise<Pagination<ProductPresentation>> {
+    const params = {
+      page: page || 1,
+      limit: limit || 10,
+      q,
+      manufacturerId: manufacturerId?.join(','),
+      categoryId: categoryId?.join(','),
+      branchId: branchId?.join(','),
+      presentationId: presentationId?.join(','),
+      priceRange: [priceRange?.min, priceRange?.max].join(','),
+    }
     const response = await this.client.get({
       url: '/product',
-      params: { page, limit, q },
+      params,
     })
 
     return response as unknown as Pagination<ProductPresentation>
