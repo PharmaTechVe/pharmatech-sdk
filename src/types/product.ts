4f562a312d4f6208df9c575569d69fb13abe6c41
Andres Alvarez <andres15alvarez@gmail.com>
2025-04-26 16:02:01 -0400

    Add stock property in product presentation response

--- src/types/product.ts
+++ src/types/product.ts
@@ -18,6 +18,7 @@ export type ProductPresentation = BaseModel & {
   price: number
   presentation: PresentationResponse
   product: Product
+  stock: number
 }
 
 export type ProductPaginationRequest = PaginationRequest & {
