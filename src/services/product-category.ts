f00fbcda7a389feadf84d76c5800175da55ec056
Andres Alvarez <andres15alvarez@gmail.com>
2025-06-07 12:43:07 -0400

    Add missing jwt in product category

--- src/services/product-category.ts
+++ src/services/product-category.ts
@@ -10,18 +10,28 @@ export class ProductCategoryService {
     this.delete = this.delete.bind(this)
   }
 
-  async create(productId: string, categoryId: string): Promise<Product> {
+  async create(
+    productId: string,
+    categoryId: string,
+    jwt: string,
+  ): Promise<Product> {
     const response = await this.client.post({
       url: `/product/${productId}/category`,
       data: { categoryId },
+      jwt,
     })
 
     return response as unknown as Product
   }
 
-  async delete(productId: string, categoryId: string): Promise<void> {
+  async delete(
+    productId: string,
+    categoryId: string,
+    jwt: string,
+  ): Promise<void> {
     await this.client.delete({
       url: `/product/${productId}/category/${categoryId}`,
+      jwt,
     })
   }
 }
